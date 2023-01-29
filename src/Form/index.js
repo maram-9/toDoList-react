4e3fcd4039c2578f45a278eda1d67dd393436e1a
Marzena Malka <marzena.malka@gmail.com>
2023-01-29 12:39:13 +0100

    prevent adding empty tasks

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,6 +6,11 @@ const Form = ({ addNewTask }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+
+        if (newTaskContent.trim() === "") {
+            return;
+          }
+          
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
     };
@@ -13,10 +18,11 @@ const Form = ({ addNewTask }) => {
     return (
     <form className="form" onSubmit={onFormSubmit}>
     <input 
+        autoFocus
         value={newTaskContent} 
         className="form__input" 
         placeholder="Co jest do zrobienia?"
-        onChange={({ target }) => setNewTaskContent(target.value)} 
+        onChange={({ target }) => setNewTaskContent(target.value)}
     />
     <button className="form__button">Dodaj zadanie</button>
 </form>
