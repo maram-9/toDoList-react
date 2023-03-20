465a285a8262217e513dba3509ae72fc16671234
Marzena Malka <marzena.malka@gmail.com>
2023-03-20 20:18:19 +0100

    install style-components and added theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        black: "#000000",
+        silver: "#CCCCCC",
+        alto: "#DDDDDD",
+        gallery: "#EEEEEE",
+        white: "#FFFFFF",
+        teal: "#008080",
+        crimson: "#DB143C",
+        forestGreen: "#228c22",
+    },
+    breakpoint: {
+        mobileMax: 767, 
+    },
+};
