d783142dcc0da83147a9910cb775a23dafaef8f9
Marzena Malka <marzena.malka@gmail.com>
2023-03-23 20:46:13 +0100

    changed paragraphs

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -13,19 +13,19 @@ export const Button = styled.button`
     transition: filter 0.3s;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    flex-basis: 100%;
-    margin: 10px;
-}
+        flex-basis: 100%;
+        margin: 10px;
+    }
 
-&:hover {
-    filter: brightness(100%);
-}
+    &:hover {
+        filter: brightness(100%);
+    }
 
-&:active {
-    filter: brightness(120%);
-}
+    &:active {
+        filter: brightness(120%);
+    }
 
-&:disabled {
-    color: ${({ theme }) => theme.color.silver};
-}
+    &:disabled {
+        color: ${({ theme }) => theme.color.silver};
+    }
 `;
