dfb05bbacbe85ee35dd7eac323685c553f3f55a4
Marzena Malka <marzena.malka@gmail.com>
2023-04-29 21:11:28 +0200

    added redux

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,48 +4,22 @@ import Section from '../../common/Section';
 import Header from '../../common/Header';
 import Container from '../../common/Container/styled';
 import TaskList from "./TaskList";
-import { useTasks } from "../../useTasks";
-//import { useSelector } from "react-redux";
 
 function Tasks() {
-  //const { tasks }= useSelector(selectTasks);
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask, 
-    toggleHideDone,
-    hideDone, 
-  } = useTasks();
-
   return (
     <Container>
      <Header title="Lista zadan" />
      <Section 
       title="Dodaj nowe zadanie" 
-      body={<Form addNewTask={addNewTask} />} 
+      body={<Form />} 
     />
      <Section
-      title="Lista zadan"
-      body={
-        <TaskList 
-        tasks={tasks} 
-        hideDone={hideDone}
-        removeTask={removeTask}
-        toggleTaskDone={toggleTaskDone} 
-        />}
-      extraHeaderContent={<
-        Buttons 
-        tasks={tasks} 
-        hideDone={hideDone}
-        toggleHideDone={toggleHideDone}
-        setAllDone={setAllDone} 
-        />}
-     />
+        title="Lista zadan"
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
+     /> 
     </Container>
   );
-}
+};
 
 export default Tasks;
