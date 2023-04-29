0869b70f27b4c58f31c023896a58eae06ea8ea92
Marzena Malka <marzena.malka@gmail.com>
2023-04-29 21:09:19 +0200

    added store

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
-import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 import { theme } from './theme';
+import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-    <App />
-    </ThemeProvider>
+        <GlobalStyle />
+          <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
