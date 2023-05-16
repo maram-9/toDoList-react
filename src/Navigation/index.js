ad0d5fc6360ee79ea53353fe1ddb22a20c0a84c8
Marzena Malka <marzena.malka@gmail.com>
2023-05-16 20:41:21 +0200

    added Navigation

---
+++ src/Navigation/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {toAuthor, toTasks} from "../routes";
+import { List, Item, StyledNavLink, Nav } from "./styled";
+
+const Navigation = () => {
+    return (
+        <Nav>
+            <List>
+                <Item>
+                    <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
+                </Item>
+                <Item>
+                    <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
+                </Item>
+            </List>
+        </Nav>
+    );
+};
+
+export default Navigation;
---
+++ src/Navigation/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const activeClassName = "link-active";
+
+export const Nav = styled.nav`
+    background-color: ${({ theme }) => theme.color.teal};
+    margin: 0;
+    padding: 0;
+`;
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({
+    activeClassName,
+}))`
+    color: ${({ theme }) => theme.color.white};
+    text-decoration: none;
+
+    &.${activeClassName} {
+        font-weight: bold;
+    }
+
+    &.hover {
+        border-bottom: 1px solid;
+    }
+    `;
+
+    export const List = styled.ul`
+        background: ${({ theme }) => theme.color.teal};
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        list-style: none;
+    `;
+
+    export const Item = styled.li`
+        margin: 20px;
+    `;
